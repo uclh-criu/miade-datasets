1eff4ef48d5d82bdd8acfbac7c8e03e17ab482c4
Anoop Shah <a.shah@ucl.ac.uk>
2024-03-04 10:39:30 +0000

    Add transitive closure table for problems. Amend pattern files.
    Created a new file 'transitive.csv' which contains:
    - ancestorId
    - descendantId
    for all ancestor / descendant pairs in the CDB. This is intended
    for 'relaxed' assessment of algorithm performance - i.e. allowing
    the most precise term or a less precise (ancestor) term.
    
    The pattern files have been amended - the human generated example
    files are no longer included in the patterns file and should not be
    used for training as they may introduce bias, because they may cause
    the algorithm to learn spurious associations.
    
    However they are useful for testing and should be used for this purpose.

--- cdb_and_model_files_sep_2023/README_medallerg.txt
+++ cdb_and_model_files_sep_2023/README_medallerg.txt
@@ -2,4 +2,5 @@ Based on May 2022 SNOMED CT.
 
 patterns_medallerg.csv contains MetaCat templated phrases using artificial random data.
 
-example1 and example2 are human generated test samples, also included in patterns_medallerg.csv
+example1 is a single human generated test sample, with multiple elements
+example2 is a set of human generated test samples
---
+++ scripts/generate_transitive_04mar2024.R
@@ -0,0 +1,36 @@
+library(Rdiagnosislist)
+library(data.table)
+SNOMED <- readRDS('~/Terminologies/SNOMED_May2022.RDS')
+
+# Load CDB
+cdb_conceptIds <- unique(SNOMEDconcept(fread(
+	'./cdb_and_model_files_sep_2023/cdb_problems.csv')$cui))
+
+# Use SNOMED relationships table to find all ancestor / descendants pairs
+SCT_isa <- SNOMEDconcept('Is a')
+
+WORKING <- SNOMED$RELATIONSHIP[(sourceId %in% cdb_conceptIds |
+	destinationId %in% cdb_conceptIds) & typeId == SCT_isa,
+	.(childId = sourceId, parentId = destinationId)]
+
+new_nrows <- nrow(WORKING)
+old_nrows <- 0
+while(new_nrows > old_nrows){
+	WORKING <- rbind(WORKING, merge(
+		WORKING[, .(childId, selfId = parentId)],
+		WORKING[, .(selfId = childId, parentId)], by = 'selfId',
+		allow.cartesian = TRUE)[,
+		.(childId, parentId)])
+	WORKING <- WORKING[!duplicated(WORKING)]
+	old_nrows <- new_nrows
+	new_nrows <- nrow(WORKING)
+}
+
+TRANSITIVE <- WORKING[childId %in% cdb_conceptIds &
+	parentId %in% cdb_conceptIds,
+	.(ancestorId = parentId, descendantId = childId)]
+
+# Export transitive table
+fwrite(TRANSITIVE,
+	file = './cdb_and_model_files_sep_2023/lookups/transitive.csv',
+	col.names = FALSE)
